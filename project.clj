567a2ada74db11a054f844fe6b7b86579c7cb85d
Juho Juopperi <juho.juopperi@remod.fi>
2014-10-05 13:49:43 +0300

    only depend on slf4j-api

--- project.clj
+++ project.clj
@@ -10,10 +10,11 @@
                  [clj-time "0.8.0"]
                  [org.clojure/java.data "0.1.1"]
                  [org.clojure/java.jdbc "0.3.5"]
-                 [org.slf4j/slf4j-simple "1.7.7"]
+                 [org.slf4j/slf4j-api "1.7.7"]
                  [org.postgis/postgis-jdbc "1.3.3"]
                  [com.taoensso/timbre "3.3.1" :exclusions [org.clojure/clojure]]
                  [prismatic/schema "0.2.6"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]
+                                  [org.slf4j/slf4j-simple "1.7.7"]]
                    :plugins [[lein-midje "3.1.1"]]}})
 
