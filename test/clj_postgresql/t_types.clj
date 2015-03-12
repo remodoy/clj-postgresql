e214e08b4cbcf47795ed1800bfa3106472232705
Juho Juopperi <juho.juopperi@remod.fi>
2015-03-13 01:24:08 +0200

    typos and make test table name random

--- test/clj_postgresql/t_types.clj
+++ test/clj_postgresql/t_types.clj
@@ -7,12 +7,14 @@
 (def test-data
   {"x" 42 "a" [4 3 2]})
 
-(facts "wirte and read json and jsonb"
+(def test-table-name (str "test_json_" (rand-int 999)))
+
+(facts "Write and read json and jsonb"
        (jdbc/with-db-transaction [tx (pg/spec)]
-         (jdbc/db-do-commands tx "CREATE TEMP TABLE test (json_field json, jsonb_field jsonb)")
-         (fact (first (jdbc/insert! tx :test {:jsonb_field test-data :json_field test-data}))
+         (jdbc/db-do-commands tx (str "CREATE TEMP TABLE " test-table-name " (json_field json, jsonb_field jsonb)"))
+         (fact (first (jdbc/insert! tx test-table-name {:jsonb_field test-data :json_field test-data}))
                => {:jsonb_field test-data :json_field test-data})
-         (let [row  (first (jdbc/query tx "SELECT * FROM test"))]
+         (let [row  (first (jdbc/query tx (str "SELECT * FROM " test-table-name)))]
            (fact row => truthy)
            (fact (:json_field row) => test-data)
            (fact (:jsonb_field row) => test-data)
