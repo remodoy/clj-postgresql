8bfd1969bd73eb8af3a435ce7387d4a905b24e70
Phil Jackson <phil@shellarchive.co.uk>
2019-05-29 17:35:58 +0100

    Update several dependencies.

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Two clause BSD license"
             :url "http://github.com/remodoy/clj-postgresql/README.md"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.postgresql/postgresql "42.1.4"]
-                 [net.postgis/postgis-jdbc "2.2.1" :exclusions [postgresql org.postgresql/postgresql]]
-                 [cheshire "5.8.0"]
+                 [org.postgresql/postgresql "42.2.5"]
+                 [net.postgis/postgis-jdbc "2.3.0" :exclusions [postgresql org.postgresql/postgresql]]
+                 [cheshire "5.8.1"]
                  [com.jolbox/bonecp "0.8.0.RELEASE"]
-                 [clj-time "0.14.2"]
+                 [clj-time "0.15.1"]
                  [org.clojure/java.data "0.1.1"]
-                 [org.clojure/java.jdbc "0.7.3"]
-                 [prismatic/schema "1.1.7"]]
-  :profiles {:dev {:dependencies [[midje "1.9.0"]
-                                  [org.slf4j/slf4j-simple "1.7.25"]]
+                 [org.clojure/java.jdbc "0.7.9"]
+                 [prismatic/schema "1.1.10"]]
+  :profiles {:dev {:dependencies [[midje "1.9.8"]
+                                  [org.slf4j/slf4j-simple "1.7.26"]]
                    :plugins [[lein-midje "3.1.1"]]}})
 
