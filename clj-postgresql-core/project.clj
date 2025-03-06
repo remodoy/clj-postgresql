423fff6bdbbcac302593f7a0b653487c6b91d5ce
Lauri Suutari <lauri.suutari@remod.fi>
2025-03-06 18:04:50 +0200

    updating dependencies

--- clj-postgresql-core/project.clj
+++ clj-postgresql-core/project.clj
@@ -4,8 +4,8 @@
   :license {:name "Two clause BSD license"
             :url "http://github.com/remodoy/clj-postgresql/README.md"}
   :scm {:dir ".."}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.postgresql/postgresql "42.7.5"]
-                 [cheshire "5.10.0"]
+                 [cheshire "5.13.0"]
                  [org.clojure/java.jdbc "0.7.11"]]
   :repl-options {:init-ns clj-postgresql.core})
--- clj-postgresql-pool/project.clj
+++ clj-postgresql-pool/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [clj-postgresql/clj-postgresql-core "1.0.1-SNAPSHOT"]
                  [hikari-cp "2.13.0"]]
   :repl-options {:init-ns clj-postgresql.pool})
