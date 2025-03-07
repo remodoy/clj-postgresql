0f60419c2d2b420134acee0369687499c7ca09eb
Lauri Suutari <lauri.suutari@remod.fi>
2025-03-07 09:49:03 +0200

    clojure to 1.12.0

--- clj-postgresql-gis/project.clj
+++ clj-postgresql-gis/project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/remodoy/clj-postgresql"
   :license {:name "Two clause BSD license"
             :url "http://github.com/remodoy/clj-postgresql/README.md"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [clj-postgresql/clj-postgresql-core "1.0.1-SNAPSHOT"]
                  [net.postgis/postgis-jdbc "2.5.0" :exclusions [postgresql org.postgresql/postgresql]]
                  [prismatic/schema "1.1.12"]]
